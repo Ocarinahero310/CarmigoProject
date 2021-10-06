b4765ada0a766b01e4de9230dd7bffc72c93b455
Ocarinahero310 <adam.tutor@yahoo.com>
2021-10-06 18:54:00 -0500

    updating frontend

--- carmigoFrontend/carmigo-frontendd/src/main.js
+++ carmigoFrontend/carmigo-frontendd/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import vuetify from './plugins/vuetify'
-import Axios from "axios";
 
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
-Vue.prototype.$http=Axios;
+
 new Vue({
   router,
   store,
